fd99babb514146847f2ecb18bf682c47b0ac175b
Rihoj <rihoj1@gmail.com>
2018-05-14 00:16:40 -0400

    This is my work.

--- CST-117-ProgrammingExercise-3/Form1.Designer.cs
+++ CST-117-ProgrammingExercise-3/Form1.Designer.cs
@@ -28,13 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.openFile = new System.Windows.Forms.OpenFileDialog();
+            this.openFileButton = new System.Windows.Forms.Button();
+            this.statsTextBox = new System.Windows.Forms.TextBox();
+            this.statsLabel = new System.Windows.Forms.Label();
+            this.saveFile = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // openFile
+            // 
+            this.openFile.FileName = "file.txt";
+            // 
+            // openFileButton
+            // 
+            this.openFileButton.Location = new System.Drawing.Point(24, 12);
+            this.openFileButton.Name = "openFileButton";
+            this.openFileButton.Size = new System.Drawing.Size(75, 23);
+            this.openFileButton.TabIndex = 0;
+            this.openFileButton.Text = "Choose File";
+            this.openFileButton.UseVisualStyleBackColor = true;
+            this.openFileButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // statsTextBox
+            // 
+            this.statsTextBox.Location = new System.Drawing.Point(24, 70);
+            this.statsTextBox.Multiline = true;
+            this.statsTextBox.Name = "statsTextBox";
+            this.statsTextBox.Size = new System.Drawing.Size(268, 103);
+            this.statsTextBox.TabIndex = 9;
+            // 
+            // statsLabel
+            // 
+            this.statsLabel.AutoSize = true;
+            this.statsLabel.Location = new System.Drawing.Point(21, 54);
+            this.statsLabel.Name = "statsLabel";
+            this.statsLabel.Size = new System.Drawing.Size(49, 13);
+            this.statsLabel.TabIndex = 10;
+            this.statsLabel.Text = "Statistics";
+            // 
+            // saveFile
+            // 
+            this.saveFile.FileName = "stats.txt";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.statsLabel);
+            this.Controls.Add(this.statsTextBox);
+            this.Controls.Add(this.openFileButton);
+            this.Name = "Form1";
+            this.Text = "File Statistics";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFile;
+        private System.Windows.Forms.Button openFileButton;
+        private System.Windows.Forms.TextBox statsTextBox;
+        private System.Windows.Forms.Label statsLabel;
+        private System.Windows.Forms.SaveFileDialog saveFile;
     }
 }
 
--- CST-117-ProgrammingExercise-3/Form1.cs
+++ CST-117-ProgrammingExercise-3/Form1.cs
@@ -7,14 +7,110 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace CST_117_ProgrammingExercise_3
 {
     public partial class Form1 : Form
     {
+        string[] words;
+        string first = null;
+        string last = null;
+        string longest = null;
+        string mostVowels = null;
+        int mostVowelsCount = 0;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                List<string> wordsInLine = new List<string>();
+                StreamReader inputFile;
+                inputFile = File.OpenText(openFile.FileName);
+                while (!inputFile.EndOfStream)
+                {
+                    foreach (string word in inputFile.ReadLine().ToLower().Split(' '))
+                    {
+                        wordsInLine.Add(word);
+                    }
+                }
+                inputFile.Close();
+                words = wordsInLine.ToArray();
+                foreach(string word in words)
+                {
+                    findAlphabetical(word);
+                    findLongestWord(word);
+                    countVowels(word);
+                }
+                string[] lines =
+                {
+                    "Alphabetically First word: "+first,
+                    "Alphabetically Last word: "+last,
+                    "Longest Word: "+longest,
+                    "Word with the most vowels: "+mostVowels
+                };
+                statsTextBox.Text = string.Join("\r\n", lines);
+                if (saveFile.ShowDialog() == DialogResult.OK)
+                {
+                    File.WriteAllLines(saveFile.FileName, lines);
+                }
+            }
+            else
+            {
+                MessageBox.Show("You clicked the Cancel button.");
+            }
+        }
+
+        private void findAlphabetical(string word)
+        {
+            if(first == null)
+            {
+                last = word;
+                first = word;
+                return;
+            }
+            if(word.CompareTo(first) == -1)
+            {
+                first = word;
+            }
+            if(word.CompareTo(last) == 1)
+            {
+                last = word;
+            }
+        }
+
+        private void findLongestWord(string word)
+        {
+            if(longest == null)
+            {
+                longest = word;
+                return;
+            }
+            if(longest.Length < word.Length)
+            {
+                longest = word;
+            }
+        }
+
+        private void countVowels(string word)
+        {
+            int count = 0;
+            for(int i = 0; i < word.Length; i++)
+            {
+                if(word[i] == 'a' || word[i] == 'e' || word[i] == 'i' || word[i] == 'o' || word[i] == 'u')
+                {
+                    count++;
+                }
+            }
+            if (count > mostVowelsCount)
+            {
+                mostVowelsCount = count;
+                mostVowels = word;
+            }
         }
     }
 }
